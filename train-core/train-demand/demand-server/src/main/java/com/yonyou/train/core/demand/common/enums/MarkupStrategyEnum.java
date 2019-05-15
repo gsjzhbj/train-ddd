7a7388a7a4bdb254b8b20bf952920df7b91f9394
gaoshja <gaoshja@yonyou.com>
2019-05-15 19:30:59 +0800

    核心领域，行程需求相关实现(加价策略）

---
+++ train-core/train-demand/demand-server/src/main/java/com/yonyou/train/core/demand/common/enums/MarkupStrategyEnum.java
@@ -0,0 +1,10 @@
+package com.yonyou.train.core.demand.common.enums;
+
+/***
+ * 加价策略
+ */
+public class MarkupStrategyEnum {
+    public static String MARKUP_STRATEGY_5="5";
+    public static String MARKUP_STRATEGY_10="10";
+    public static String MARKUP_STRATEGY_20="20";
+}
