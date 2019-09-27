391416c8fe4e802b672712879d73879bd0e4d029
gaoshja <gaoshja@yonyou.com>
2019-09-27 15:47:22 +0800

    9月27日提交

---
+++ train-core/train-order/order-server/src/main/java/com/yonyou/train/order/application/service/OrderItemService.java
@@ -0,0 +1,12 @@
+package com.yonyou.train.order.application.service;
+
+import com.yonyou.train.order.domain.vo.OrderVo;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderItemService implements com.yonyou.train.order.interfaces.IOrderItemService {
+    @Override
+    public void createOrderItems(OrderVo orderVo) {
+
+    }
+}
